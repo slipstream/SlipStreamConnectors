ea92b2b45c7173183eafe7e5336f72373de14e26
schaubl <lionel.schaub@bluewin.ch>
2014-10-29 01:27:00 +0100

    See https://github.com/slipstream/SlipStream/issues/19.
    Rollback: slipstream/SlipStreamServer@863c0174711b8ab84277275c9ac8b63f8f7e6014

--- cloudstack/jar/src/main/java/com/sixsq/slipstream/connector/cloudstack/CloudStackConnector.java
+++ cloudstack/jar/src/main/java/com/sixsq/slipstream/connector/cloudstack/CloudStackConnector.java
@@ -343,7 +343,7 @@ public class CloudStackConnector extends CliConnectorBase {
 		userData += "export SLIPSTREAM_VERBOSITY_LEVEL=\"" + getVerboseParameterValue(user) + "\"\n";
 
 		userData += "mkdir -p " + SLIPSTREAM_REPORT_DIR + "\n"
-				+ "wget --secure-protocol=SSLv3 --no-check-certificate -O " + bootstrap
+				+ "wget --no-check-certificate -O " + bootstrap
 				+ " $SLIPSTREAM_BOOTSTRAP_BIN > " + SLIPSTREAM_REPORT_DIR + "/"
 				+ logfilename + " 2>&1 " + "&& chmod 0755 " + bootstrap + "\n"
 				+ bootstrap + " " + targetScript + " >> "
