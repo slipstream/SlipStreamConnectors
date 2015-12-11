20d41dfb9f4758df2cc3b8768503728e5b22d0aa
khaled <khaled@sixsq.com>
2015-12-11 21:10:55 +0100

    single component deployment working

--- opennebula/python/tar/slipstream_opennebula/OpenNebulaClientCloud.py
+++ opennebula/python/tar/slipstream_opennebula/OpenNebulaClientCloud.py
@@ -26,6 +26,13 @@ from slipstream.util import override
 from slipstream.NodeDecorator import NodeDecorator
 from slipstream.cloudconnectors.BaseCloudConnector import BaseCloudConnector
 
+import xmlrpclib
+import urllib
+from pprint import pprint, pformat
+import xml.etree.ElementTree as etree
+import re
+import base64
+
 def getConnector(configHolder):
     return getConnectorClass()(configHolder)
 
@@ -54,14 +61,23 @@ class OpenNebulaClientCloud(BaseCloudConnector):
 
         self._set_capabilities(contextualization=True,
                                orchestrator_can_kill_itself_or_its_vapp=True)
-
         self.flavors = []
         self.images = []
         self.tempPrivateKey = None
+        self.user_info = None
+
+    def _rpc_execute(self, command, *args):
+        remoteFunction = getattr(self.driver, command)
+        success, output_or_error_msg, err_code = remoteFunction(self._createSessionString(self.user_info), *args)
+        if not success:
+            raise Exceptions.ExecutionException(output_or_error_msg)
+        return output_or_error_msg
 
     @override
     def _initialization(self, user_info):
         util.printStep('Initialize the OpenNebula connector.')
+        self.driver = self._createRpcConnection(user_info)
+        self.user_info = user_info
         # self._thread_local.driver = self._get_driver(user_info)
         # self.flavors = self._thread_local.driver.list_sizes()
         # self.images = self._thread_local.driver.list_images()
@@ -118,17 +134,52 @@ class OpenNebulaClientCloud(BaseCloudConnector):
         # return newImg.id
         return None
 
+    def format_instance_name(self, name):
+        name = self.remove_bad_char_in_instance_name(name)
+        return self.truncate_instance_name(name)
+
+    def truncate_instance_name(self, name):
+        if len(name) <= 128:
+            return name
+        else:
+            return name[:63] + '-' + name[-63:]
+
+    def remove_bad_char_in_instance_name(self, name):
+        return re.sub('[^a-zA-Z0-9-]', '', name)
+
     @override
     def _start_image(self, user_info, node_instance, vm_name):
         util.printStep('KB: _start_image.')
-        #self._thread_local.driver = self._get_driver(user_info)
-        return self._start_image_on_opennebula(user_info, node_instance, vm_name)
+        return self._start_image_on_opennebula(node_instance, vm_name)
 
-    def _start_image_on_opennebula(self, user_info, node_instance, vm_name):
+    def _start_image_on_opennebula(self, node_instance, vm_name):
         util.printStep('KB: _start_image_on_opennebula.')
-        # image_id = node_instance.get_image_id()
-        # instance_type = node_instance.get_instance_type()
-        # keypair = user_info.get_keypair_name()
+        image_id = node_instance.get_image_id()
+        #instance_type = int(node_instance.get_instance_type())
+        contextualization_script = self.is_build_image() and '' or self._get_bootstrap_script(node_instance)
+        with open("/tmp/debug", "a") as myfile:
+            myfile.write(contextualization_script)
+            myfile.write('\n\nbase64\n')
+            myfile.write(base64.b64encode(contextualization_script))
+        disk = ' DISK = [ IMAGE_ID  = 8, SIZE = 20000 ]'
+        nic = ' NIC = [ NETWORK = "public", NETWORK_UNAME="oneadmin" ]'
+        context = ' CONTEXT = [ NETWORK = "YES", SSH_PUBLIC_KEY = "' + self.user_info.get_public_keys() + '", START_SCRIPT_BASE64 = "%s"]' % base64.b64encode(contextualization_script)
+        template = 'INSTANCE_TYPE = "micro" VCPU = 1 CPU = 0.5 MEMORY = 512 Name = ' + self.format_instance_name(vm_name) + disk + nic + context
+        vm_id = self._rpc_execute('one.vm.allocate', template, False)
+        vm = self._rpc_execute('one.vm.info', vm_id)
+        #xml = etree.fromstring(vm)
+        #pprint('ici:')
+        #pprint(xml.find('TEMPLATE/NIC/IP').text)
+        #vm = self._get_from_rpc(user_info, 'one.template.instantiate', instance_type, vm_name, False, 'NIC = [ NETWORK = "private" ] DISK = [ IMAGE_ID  = 10 ]')
+        #success, vm_id_or_error_msg, error_code = self.driver.one.template.instantiate(self._createSessionString(user_info), instance_type, vm_name, False, "")
+        #pprint("vm_id_or_error_msg:")
+        #pprint(vm_id_or_error_msg)
+        #pprint("success:")
+        #pprint(success)
+        #pprint("error_code:")
+        #pprint(error_code)
+
+        #keypair = user_info.get_keypair_name()
         # #security_groups = self._get_security_groups_for_node_instance(node_instance)
         # flavor = searchInObjectList(self.flavors, 'name', instance_type)
         # image = searchInObjectList(self.images, 'id', image_id)
@@ -161,19 +212,30 @@ class OpenNebulaClientCloud(BaseCloudConnector):
         #
         # instance = self._thread_local.driver.create_node(**kwargs)
         #
-        # vm = dict(networkType=node_instance.get_network_type(),
-        #           instance=instance,
-        #           ip='',
-        #           id=instance.id)
-        # return vm
-        return None
+        #vm = dict(networkType=node_instance.get_network_type(),
+        #          instance='test',
+        #          ip='',
+        #          id=1)
+        #return vm
+        return etree.fromstring(vm)
 
+    def format_instance_name(self, name):
+        name = self.remove_bad_char_in_instance_name(name)
+        return self.truncate_instance_name(name)
+
+    def truncate_instance_name(self, name):
+        if len(name) <= 128:
+            return name
+        else:
+            return name[:63] + '-' + name[-63:]
+
+    def remove_bad_char_in_instance_name(self, name):
+        return re.sub('[^a-zA-Z0-9-]', '', name)
 
     @override
     def list_instances(self):
-        util.printStep('KB: list_instances.')
-        #return self._thread_local.driver.list_nodes()
-        return None
+        vms = etree.fromstring(self._rpc_execute('one.vmpool.info', -3, -1, -1, 3))
+        return vms.findall('VM')
 
     @override
     def _stop_deployment(self):
@@ -183,159 +245,47 @@ class OpenNebulaClientCloud(BaseCloudConnector):
 
     @override
     def _stop_vms_by_ids(self, ids):
-        util.printStep('KB: _stop_vms_by_ids.')
-        # for node in self._thread_local.driver.list_nodes():
-        #     if node.id in ids:
-        #         node.destroy()
+        for _id in map(int, ids):
+            self._rpc_execute('one.vm.action', 'delete', _id)
 
-    def _get_driver(self, user_info):
-        util.printStep('KB: _get_driver.')
-        # driverOpenStack = get_driver(Provider.OPENSTACK)
-        # isHttps = user_info.get_cloud_endpoint().lower().startswith('https://')
-        #
-        # return driverOpenStack(user_info.get_cloud_username(),
-        #                        user_info.get_cloud_password(),
-        #                        secure=isHttps,
-        #                        ex_tenant_name=user_info.get_cloud('tenant.name'),
-        #                        ex_force_auth_url=user_info.get_cloud_endpoint(),
-        #                        ex_force_auth_version='2.0_password',
-        #                        ex_force_service_type=user_info.get_cloud('service.type'),
-        #                        ex_force_service_name=user_info.get_cloud('service.name'),
-        #                        ex_force_service_region=user_info.get_cloud('service.region'))
-        return None
+    def _createSessionString(self, user_info):
+        quotedUsername = urllib.quote(user_info.get_cloud_username(), '')
+        quotedPassword = urllib.quote(user_info.get_cloud_password(), '')
+        return '%s:%s' % (quotedUsername, quotedPassword)
+
+    def _createRpcConnection(self, user_info):
+        protocolSeparator = '://'
+        parts = user_info.get_cloud_endpoint().split(protocolSeparator)
+        url = parts[0] + protocolSeparator + self._createSessionString(user_info) + "@" + ''.join(parts[1:])
+        return xmlrpclib.ServerProxy(url)
 
     @override
     def _vm_get_ip(self, vm):
-        util.printStep('KB: _vm_get_ip.')
-        #return vm['ip']
-        return None
+        return vm.findtext('TEMPLATE/NIC/IP')
 
     @override
     def _vm_get_id(self, vm):
-        util.printStep('KB: _vm_get_id.')
-        #return vm['id']
-        return None
-
-    def _get_vm_size(self, vm_instance):
-        util.printStep('KB: _get_vm_size.')
-        # try:
-        #     size = [i for i in self.flavors if i.id == vm_instance.extra.get('flavorId')][0]
-        # except IndexError:
-        #     return None
-        # else:
-        #     return size
-        return None
+        return vm.findtext('ID')
 
     @override
     def _vm_get_ip_from_list_instances(self, vm_instance):
-        util.printStep('KB: _vm_get_ip_from_list_instances.')
-        #return self._get_instance_ip_address(vm_instance)
-        return None
+        return self._vm_get_ip(vm_instance)
 
     @override
     def _vm_get_cpu(self, vm_instance):
-        util.printStep('KB: _vm_get_cpu.')
-        # size = self._get_vm_size(vm_instance)
-        # if size:
-        #     return size.vcpus
-        return None
+        return vm_instance.findtext('TEMPLATE/VCPU')
 
     @override
     def _vm_get_ram(self, vm_instance):
-        util.printStep('KB: _vm_get_ram.')
-        # size = self._get_vm_size(vm_instance)
-        # if size:
-        #     return size.ram
-        return None
+        return vm_instance.findtext('TEMPLATE/MEMORY')
 
     @override
     def _vm_get_root_disk(self, vm_instance):
-        util.printStep('KB: _vm_get_root_disk.')
-        # size = self._get_vm_size(vm_instance)
-        # if size:
-        #     return size.disk
-        return None
+        return vm_instance.findtext('TEMPLATE/DISK/SIZE')
 
     @override
     def _vm_get_instance_type(self, vm_instance):
-        util.printStep('KB: _vm_get_instance_type.')
-        # size = self._get_vm_size(vm_instance)
-        # if size:
-        #     return size.name
-        return None
-
-    def _get_instance_ip_address(self, instance, ip_type='', strict=True):
-        util.printStep('KB: _get_instance_ip_address.')
-        # if ip_type.lower() == 'private':
-        #     return (len(instance.private_ips) != 0) and instance.private_ips[0] or (len(instance.public_ips) != 0 and not strict) and instance.public_ips[0] or ''
-        # elif ip_type.lower() == 'public':
-        #     return (len(instance.public_ips) != 0) and instance.public_ips[0] or (len(instance.private_ips) != 0 and not strict) and instance.private_ips[0] or ''
-        # else:
-        #     return (len(instance.public_ips) != 0) and instance.public_ips[0] or (len(instance.private_ips) != 0) and instance.private_ips[0] or ''
-        return None
-
-    @override
-    def _wait_and_get_instance_ip_address(self, vm):
-        util.printStep('KB: _wait_and_get_instance_ip_address.')
-        # timeWait = 300
-        # timeStop = time.time() + timeWait
-        #
-        # while time.time() < timeStop:
-        #     time.sleep(1)
-        #
-        #     ipType = vm['networkType']
-        #     vmId = vm['id']
-        #
-        #     instances = self._thread_local.driver.list_nodes()
-        #     instance = searchInObjectList(instances, 'id', vmId)
-        #     ip = self._get_instance_ip_address(instance, ipType or '')
-        #     if ip:
-        #         vm['ip'] = ip
-        #         return vm
-        #
-        # try:
-        #     ip = self._get_instance_ip_address(instance, ipType or '', False)
-        # # pylint: disable=W0703
-        # except Exception:
-        #     pass
-        #
-        # if ip:
-        #     vm['ip'] = ip
-        #     return vm
-        #
-        # raise Exceptions.ExecutionException(
-        #     'Timed out while waiting for IPs to be assigned to instances: %s' % vmId)
-        return None
-
-    def _wait_instance_in_running_state(self, instanceId):
-        util.printStep('KB: _wait_instance_in_running_state.')
-        # timeWait = 300
-        # timeStop = time.time() + timeWait
-        #
-        # state = ''
-        # while state != NodeState.RUNNING:
-        #     if time.time() > timeStop:
-        #         raise Exceptions.ExecutionException(
-        #             'Timed out while waiting for instance "%s" enter in running state'
-        #             % instanceId)
-        #     time.sleep(1)
-        #     node = self._thread_local.driver.list_nodes()
-        #     state = searchInObjectList(node, 'id', instanceId).state
-        return None
-
-    def _wait_image_creation_completed(self, imageId):
-        util.printStep('KB: _wait_image_creation_completed.')
-        # timeWait = 600
-        # timeStop = time.time() + timeWait
-        #
-        # imgState = None
-        # while imgState == None:
-        #     if time.time() > timeStop:
-        #         raise Exceptions.ExecutionException(
-        #             'Timed out while waiting for image "%s" to be created' % imageId)
-        #     time.sleep(1)
-        #     images = self._thread_local.driver.list_images()
-        #     imgState = searchInObjectList(images, 'id', imageId)
+        return vm_instance.findtext('USER_TEMPLATE/INSTANCE_TYPE')
 
     def _import_keypair(self, user_info):
         util.printStep('KB: _import_keypair.')
