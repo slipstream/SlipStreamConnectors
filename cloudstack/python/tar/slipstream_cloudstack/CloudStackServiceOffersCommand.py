6f29c9f9ea6f4144ca3875c5d88d5a3a7741b5c0
khaled basbous <khaled@sixsq.com>
2017-05-09 12:05:59 +0200

    able to extract all service offers with windows and different disk sizes

--- cloudstack/python/tar/slipstream_cloudstack/CloudStackServiceOffersCommand.py
+++ cloudstack/python/tar/slipstream_cloudstack/CloudStackServiceOffersCommand.py
@@ -56,10 +56,57 @@ class ServiceOffersInstancesCommand(CloudClientCommand):
               return float(x['Rate'])
         return None
 
-    def _get_price(self, instanceType):
-        priceChf = float(self.prices['running_' + instanceType.lower().replace("-", "_")])
-        priceEur = priceChf * self._get_currency_exchange_rate('CHFEUR')
-        return priceEur 
+    def _get_price(self, instanceType, os, diskSizeGiB):
+        instanceName = 'running_' + instanceType.lower().replace("-", "_")
+        if os == "windows":
+          priceChf = float(self.prices.get(instanceName + '_win', -1))
+        else:
+          priceChf = float(self.prices.get(instanceName))
+        if (priceChf >= 0):
+            priceEur = (priceChf + diskSizeGiB * float(self.prices.get('volume'))) * self._get_currency_exchange_rate('CHFEUR')
+            return priceEur
+        else:
+            return None
+
+    def get_disk_sizes(self, instanceType):
+        defaultDiskSizes = [10, 50, 100, 200, 400]
+        diskSizes = {"Micro": [10, 50, 100, 200],
+                     "Tiny": defaultDiskSizes,
+                     "Small": defaultDiskSizes, 
+                     "Medium": defaultDiskSizes,
+                     "Large": defaultDiskSizes,
+                     "Extra-large": defaultDiskSizes,
+                     "Huge": defaultDiskSizes,
+                     "Mega": [10, 50, 100, 200, 400, 800],
+                     "Titan": [10, 50, 100, 200, 400, 800, 1600],
+                     "GPU-small": [100, 200, 400, 800, 1600, 2000], 
+                     "GPU-huge": [100, 200, 400, 800, 1600, 2000, 8000]}
+        return diskSizes.get(instanceType, defaultDiskSizes)   
+    
+    def generateServiceOffer(self, instanceTypeName, zone, cpuSize, ramSizeMiB, diskSizeGiB, os, price):
+        country = 'CH'
+        resourceType = 'VM'
+        resourceClass = 'standard'
+        return {"name": "{:d}/{:d}/{:d} [{}] ({})".format(cpuSize,ramSizeMiB, diskSizeGiB, country, instanceTypeName),
+                "description": "{} ({}) with {:d} vCPU, {:d} MiB RAM, {:d} GiB disk [{}, {}] ({})"
+                                   .format(resourceType, resourceClass, cpuSize, ramSizeMiB, diskSizeGiB, zone, country, instanceTypeName),
+                "resource:vcpu": cpuSize,
+                "resource:ram": ramSizeMiB,
+                "resource:disk": diskSizeGiB,
+                "resource:diskType": "SSD",
+                "resource:type": resourceType,
+                "resource:class": resourceClass,
+                "resource:country": country,
+                "resource:platform": "cloudstack",
+                "resource:operatingSystem": os,
+                "price:unitCost": price,
+                "price:unitCode": "C62",
+                "price:freeUnits": 0,
+                "price:currency": "EUR",
+                "price:billingUnitCode": "HUR",
+                "price:billingPeriodCode": "MIN",
+                "exoscale:instanceType": instanceTypeName,
+                "exoscale:zone": zone}
 
     def do_work(self):
         ch = ConfigHolder(options={'verboseLevel': 0,
@@ -70,10 +117,32 @@ class ServiceOffersInstancesCommand(CloudClientCommand):
         cc._initialization(self.user_info, **self.get_initialization_extra_kwargs())
 
         for instanceType in cc.sizes:
-            print "name:{} cpu:{:d} ram:{:d} disk:{:d}, price:{:f}".format(instanceType.name, instanceType.extra['cpu'], instanceType.ram, instanceType.disk, self._get_price(instanceType.name))
+            instanceTypeName = instanceType.name
+            for diskSizeGiB in self.get_disk_sizes(instanceTypeName):
+                for os in ["linux", "windows"]:
+                    price = self._get_price(instanceType.name, os, diskSizeGiB)
+                    if (price is None or (os == "windows" and diskSizeGiB < 50)):
+                        continue
+                    zone = self.get_option(self.ZONE_KEY)
+                    cpuSize = int(instanceType.extra['cpu'])
+                    ramSizeMiB = int(instanceType.ram)
+                    serviceOffer = self.generateServiceOffer(instanceTypeName, zone, cpuSize, ramSizeMiB, diskSizeGiB, os, price)
+                    print serviceOffer
 
 class CloudStackServiceOffers(ServiceOffersInstancesCommand, CloudStackCommand):
+
+    CONNECTOR_NAME_KEY = 'connector-name'
 
     def __init__(self):
         super(CloudStackServiceOffers, self).__init__()
 
+    def set_cloud_specific_options(self, parser):
+        CloudStackCommand.set_cloud_specific_options(self, parser)
+
+        self.parser.add_option('--' + self.CONNECTOR_NAME_KEY, dest=self.CONNECTOR_NAME_KEY,
+                               help='Connector name to be used as a connector href for service offers)',
+                               default=None, metavar='exoscale-ch-gva')
+
+    def get_cloud_specific_mandatory_options(self):
+        return CloudStackCommand.get_cloud_specific_mandatory_options(self) + \
+               [self.CONNECTOR_NAME_KEY]
