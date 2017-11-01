f1ff27ac2b98d1877b7871be6ee5520a8ba89aed
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2017-11-01 13:42:17 +0100

    updated to new test utils namespace

--- cloudstack/config/test/com/sixsq/slipstream/ssclj/resources/credential_cloud_cloudstack_lifecycle_test.clj
+++ cloudstack/config/test/com/sixsq/slipstream/ssclj/resources/credential_cloud_cloudstack_lifecycle_test.clj
@@ -6,7 +6,7 @@
     [com.sixsq.slipstream.ssclj.resources.common.dynamic-load :as dyn]
     [com.sixsq.slipstream.ssclj.resources.lifecycle-test-utils :as ltu]
     [com.sixsq.slipstream.ssclj.resources.credential-template :as ct]
-    [com.sixsq.slipstream.ssclj.resources.credential-cloud-lifecycle-test :as cclt]
+    [com.sixsq.slipstream.ssclj.resources.credential-cloud-lifecycle-test-utils :as cclt]
     [com.sixsq.slipstream.ssclj.resources.credential-template-cloud-cloudstack :as cloud-cloudstack]))
 
 (use-fixtures :each ltu/with-test-es-client-fixture)
--- stratuslab/config/test/com/sixsq/slipstream/ssclj/resources/credential_cloud_stratuslabiter_lifecycle_test.clj
+++ stratuslab/config/test/com/sixsq/slipstream/ssclj/resources/credential_cloud_stratuslabiter_lifecycle_test.clj
@@ -4,7 +4,7 @@
     [peridot.core :refer :all]
     [clojure.data.json :as json]
     [com.sixsq.slipstream.ssclj.resources.credential-template-cloud-stratuslabiter :as cloud-stratuslabiter]
-    [com.sixsq.slipstream.ssclj.resources.credential-cloud-lifecycle-test :as cclt]
+    [com.sixsq.slipstream.ssclj.resources.credential-cloud-lifecycle-test-utils :as cclt]
     [com.sixsq.slipstream.ssclj.resources.credential-template :as ct]
     [com.sixsq.slipstream.ssclj.resources.lifecycle-test-utils :as ltu]
     [com.sixsq.slipstream.ssclj.resources.common.dynamic-load :as dyn]))
