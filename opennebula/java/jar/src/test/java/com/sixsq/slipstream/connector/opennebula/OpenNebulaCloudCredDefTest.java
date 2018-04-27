433f2353d9aaa4b37e0b487be9c2fe0a4baa2e56
Eric Le Goff <elegoff@sixsq.com>
2018-04-27 18:13:25 +0200

    Avoid hardcoded CONNECTOR_NAME

--- opennebula/java/jar/src/test/java/com/sixsq/slipstream/connector/opennebula/OpenNebulaCloudCredDefTest.java
+++ opennebula/java/jar/src/test/java/com/sixsq/slipstream/connector/opennebula/OpenNebulaCloudCredDefTest.java
@@ -20,7 +20,7 @@ public class OpenNebulaCloudCredDefTest extends CloudCredDefTestBase {
                     OpenNebulaSystemConfigurationParametersFactory(getConnectorName());
         } catch (ValidationException e) {
             e.printStackTrace();
-            fail("Failed to create connector " + CONNECTOR_NAME + " with: " +
+            fail("Failed to create connector " + getConnectorName() + " with: " +
                     e.getMessage());
         }
         runCloudCredentialsDirectLifecycle(
