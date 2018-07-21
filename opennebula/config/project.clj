4109eb614658975666275d4957800d5e2c7222be
sixsq-hudson <hudson@sixsq.com>
2018-07-21 16:12:30 +0000

    release v3.55

--- opennebula/config/project.clj
+++ opennebula/config/project.clj
@@ -1,6 +1,6 @@
-(def +version+ "3.55-SNAPSHOT")
+(def +version+ "3.55")
 
-(defproject com.sixsq.slipstream/SlipStreamConnector-OpenNebula-conf "3.55-SNAPSHOT"
+(defproject com.sixsq.slipstream/SlipStreamConnector-OpenNebula-conf "3.55"
 
   :description "OpenNebula connector configuration"
 
--- openstack/config/project.clj
+++ openstack/config/project.clj
@@ -1,6 +1,6 @@
-(def +version+ "3.55-SNAPSHOT")
+(def +version+ "3.55")
 
-(defproject com.sixsq.slipstream/SlipStreamConnector-OpenStack-conf "3.55-SNAPSHOT"
+(defproject com.sixsq.slipstream/SlipStreamConnector-OpenStack-conf "3.55"
 
   :description "OpenStack connector configuration"
 
