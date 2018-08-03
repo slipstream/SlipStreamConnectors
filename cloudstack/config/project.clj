918846eaa4713bd8e9f1fe8cce589e9ce945557b
sixsq-hudson <hudson@sixsq.com>
2018-08-03 09:13:11 +0000

    release v3.56

--- cloudstack/config/project.clj
+++ cloudstack/config/project.clj
@@ -1,6 +1,6 @@
-(def +version+ "3.56-SNAPSHOT")
+(def +version+ "3.56")
 
-(defproject com.sixsq.slipstream/SlipStreamConnector-CloudStack-conf "3.56-SNAPSHOT"
+(defproject com.sixsq.slipstream/SlipStreamConnector-CloudStack-conf "3.56"
 
   :description "CloudStack connector configuration"
 
