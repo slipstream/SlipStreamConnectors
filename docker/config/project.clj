71dd4c564d4497e3f4260088c9eb1bd951adb799
sixsq-hudson <hudson@sixsq.com>
2018-10-12 15:25:41 +0000

    release v3.61

--- docker/config/project.clj
+++ docker/config/project.clj
@@ -1,6 +1,6 @@
-(def +version+ "3.61-SNAPSHOT")
+(def +version+ "3.61")
 
-(defproject com.sixsq.slipstream/SlipStreamConnector-Docker-conf "3.61-SNAPSHOT"
+(defproject com.sixsq.slipstream/SlipStreamConnector-Docker-conf "3.61"
 
   :description "Docker connector configuration"
 
--- openstack/config/project.clj
+++ openstack/config/project.clj
@@ -1,6 +1,6 @@
-(def +version+ "3.61-SNAPSHOT")
+(def +version+ "3.61")
 
-(defproject com.sixsq.slipstream/SlipStreamConnector-OpenStack-conf "3.61-SNAPSHOT"
+(defproject com.sixsq.slipstream/SlipStreamConnector-OpenStack-conf "3.61"
 
   :description "OpenStack connector configuration"
 
