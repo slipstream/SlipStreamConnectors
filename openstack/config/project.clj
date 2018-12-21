792787069158ca708d36f93d4fac78073e831c9a
sixsq-hudson <hudson@sixsq.com>
2018-12-21 10:49:02 +0000

    release v3.66

--- openstack/config/project.clj
+++ openstack/config/project.clj
@@ -1,6 +1,6 @@
-(def +version+ "3.66-SNAPSHOT")
+(def +version+ "3.66")
 
-(defproject com.sixsq.slipstream/SlipStreamConnector-OpenStack-conf "3.66-SNAPSHOT"
+(defproject com.sixsq.slipstream/SlipStreamConnector-OpenStack-conf "3.66"
 
   :description "OpenStack connector configuration"
 
