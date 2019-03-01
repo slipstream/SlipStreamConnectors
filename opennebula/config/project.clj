6f3e1f00badade99ebd3697597f5aad6735ea57e
sixsq-hudson <hudson@sixsq.com>
2019-03-01 12:31:34 +0000

    next development version

--- opennebula/config/project.clj
+++ opennebula/config/project.clj
@@ -1,6 +1,6 @@
-(def +version+ "3.70")
+(def +version+ "3.71-SNAPSHOT")
 
-(defproject com.sixsq.slipstream/SlipStreamConnector-OpenNebula-conf "3.70"
+(defproject com.sixsq.slipstream/SlipStreamConnector-OpenNebula-conf "3.71-SNAPSHOT"
 
   :description "OpenNebula connector configuration"
 
